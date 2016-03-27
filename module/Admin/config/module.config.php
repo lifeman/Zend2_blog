34b8a09aa2a70819f1ee8e2efb91fb58ae86242a
lifeman <lifeman.web@gmail.com>
2016-03-27 22:08:55 +0300

    task 10

---
+++ module/Admin/config/module.config.php
@@ -0,0 +1,41 @@
+<?php
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Admin\Controller\Index' => 'Admin\Controller\IndexController',
+            'category' => 'Admin\Controller\CategoryController',
+        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'admin' => array(
+                'type' => 'literal',
+                'options' => array(
+                    'route' => '/admin/',
+                    'defaults' => array(
+                        'controller' => 'Admin\Controller\Index',
+                        'action' => 'index',
+                    )
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'category' => array(
+                        'type' => 'segment',
+                        'options' => array(
+                            'route' => 'category/[:action/][:id/]',
+                            'defaults' => array(
+                                'controller' => 'category',
+                                'action' => 'index',
+                            )
+                        )
+                    )
+                ),// chield
+            ),
+        )
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        )
+    )
+);
